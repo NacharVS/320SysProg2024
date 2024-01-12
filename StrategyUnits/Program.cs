eaf3d695c9c19532ba436877ae8628b601f5a76c
r3dinc <r3dinc@yandex.ru>
2024-01-12 09:26:47 +0300

    units draft

--- StrategyUnits/Program.cs
+++ StrategyUnits/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using StrategyUnits;
+
+Footman footman = new Footman();
+Footman footman2 = new Footman();
+Peasant ps1 = new Peasant();
+
+ps1.ShowInfo();
+footman2.ShowInfo();
+footman.InflictDamage(ps1);
+footman.InflictDamage(footman2);
+ps1.ShowInfo();
+footman2.ShowInfo();
