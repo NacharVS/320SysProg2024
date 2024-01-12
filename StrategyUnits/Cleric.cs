f28ccd854070386d5fb736311c416d989992078b
c3n9 <arseniy11041104@gmail.com>
2024-01-12 11:08:08 +0300

    logic for heal

---
+++ StrategyUnits/Cleric.cs
@@ -0,0 +1,84 @@
+﻿namespace StrategyUnits
+{
+    internal class Cleric : Unit
+    {
+        private int _maxEnergy;
+        private int _currentEnergy;
+
+        public int CurrentEnergy
+        {
+            get { return _currentEnergy; }
+            set 
+            { 
+                if(value < 0)
+                {
+                    _currentEnergy = 0;
+                }
+                else
+                {
+                    if (value > MaxEnergy)
+                        _currentEnergy = MaxEnergy;
+                    else 
+                        _currentEnergy = value;
+                }
+            }
+        }
+
+        public int MaxEnergy
+        {
+            get { return _maxEnergy; }
+            set { _maxEnergy = value; }
+        }
+        public Cleric() : base(10, "Cleric")
+        {
+            _maxEnergy = 3;
+            _currentEnergy = _maxEnergy;
+        }
+
+        public void HealSelf(Unit unit)
+        {
+            var healthBeforeHealth = unit.CurrentHealth;
+            if (unit.IsDied)
+            {
+                Console.WriteLine($"{unit.Name} умер, восстановить здоровье нельзя") ;
+                return;
+            }
+            while (_currentEnergy > 0)
+            {
+                if (unit.MaxHealth == unit.CurrentHealth)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"{unit.Name} исцелился с {healthBeforeHealth} до {unit.CurrentHealth}");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+                }
+                unit.CurrentHealth += 2;
+                _currentEnergy -= 1;
+            }
+        }
+        public void HealSomebody(Unit unit)
+        {
+            var healthBeforeHealth = unit.CurrentHealth;
+            if (unit.IsDied)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"{unit.Name} умер, восстановить здоровье нельзя");
+                Console.ForegroundColor = ConsoleColor.White;
+                return;
+            }
+            while (_currentEnergy > 0)
+            {
+                if (unit.MaxHealth == unit.CurrentHealth)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"{unit.Name} исцелился с {healthBeforeHealth} до {unit.CurrentHealth}");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+                }
+                unit.CurrentHealth += 1;
+                _currentEnergy -= 2;
+            }
+        }
+
+    }
+}
