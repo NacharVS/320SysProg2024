706daef1b3933751cf82600f16097b04dd8d6c3a
EelvinafayY <elvinafayzullina2005@icloud.com>
2024-01-12 11:06:22 +0300

    Начало практики

---
+++ StrategyUnits/Cleric.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyUnits
+{
+    internal class Cleric : Unit
+    {
+        private int _energy;
+
+        private int _selfheal;
+        public int SelfHeal
+        {
+            get { return _selfheal; }
+            set { _selfheal = value; }
+        }
+        private int _healother;
+        public int Healother
+        {
+            get { return _healother; }
+            set { _healother = value; }
+        }
+
+        public Cleric() : base(60, "Cleric")
+        {
+           _energy = 100;
+           _selfheal = 2;
+           _healother= 1;
+        }
+
+        public void InflictHeal (Unit unit)
+        {
+            while (_energy > 0)
+            {
+               
+                if (unit.Health >= MaxHealth)
+                {
+                    unit.Health = MaxHealth;
+                }
+                else if (unit.Health <= 0) 
+                {
+                    Console.WriteLine("Персонаж мертв! Лечить нельзя!");
+                }
+                else
+                {
+                    _energy -= 2;
+                    unit.Health += _healother;
+                }
+                
+            }
+        }
+    }
+}
--- StrategyUnits/Program.cs
+++ StrategyUnits/Program.cs
@@ -3,10 +3,13 @@
 Footman footman = new Footman();
 Footman footman2 = new Footman();
 Peasant ps1 = new Peasant();
+Cleric cleric = new Cleric();
 
 ps1.ShowInfo();
-footman2.ShowInfo();
 footman.InflictDamage(ps1);
-footman.InflictDamage(footman2);
+footman.InflictDamage(ps1);
+footman.InflictDamage(ps1);
+footman.InflictDamage(ps1);
+footman.InflictDamage(ps1);
+cleric.InflictHeal(ps1);
 ps1.ShowInfo();
-footman2.ShowInfo();
