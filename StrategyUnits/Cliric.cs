516e58888aa54b9d700f737979710ffbf50e9aa6
Egor <Egor@DESKTOP-P5K0VAL>
2024-01-12 10:53:21 +0300

    Только клирик

---
+++ StrategyUnits/Cliric.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace StrategyUnits
+{
+    internal class Cliric: Unit
+    {
+            private int _healthValue;
+
+            public int Heal
+            {
+                get { return _healthValue; }
+                set { _healthValue = value; }
+            }
+
+            public Cliric() : base(80, "Clirc")
+            {
+                _healthValue = 10;
+            }
+
+            public void InflictHeal(Unit unit)
+            {
+                unit.Health += _healthValue;
+            }
+
+        
+    }
+}
--- StrategyUnits/Program.cs
+++ StrategyUnits/Program.cs
@@ -3,10 +3,13 @@
 Footman footman = new Footman();
 Footman footman2 = new Footman();
 Peasant ps1 = new Peasant();
+Cliric cl1 = new Cliric();
 
 ps1.ShowInfo();
 footman2.ShowInfo();
 footman.InflictDamage(ps1);
-footman.InflictDamage(footman2);
+//footman.InflictDamage(footman2);
 ps1.ShowInfo();
-footman2.ShowInfo();
+cl1.InflictHeal(ps1);
+ps1.ShowInfo();
+//footman2.ShowInfo();
