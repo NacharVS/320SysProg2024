df58505539e135c24a5093647b1bdc3b57cb48db
Flectere <clashr97@mail.ru>
2024-01-12 11:05:42 +0300

    start

--- StrategyUnits/Footman.cs
+++ StrategyUnits/Footman.cs
@@ -17,7 +17,15 @@
 
         public void InflictDamage(Unit unit)
         {
-            unit.Health -= _damage;
+            if (unit.isAlive)
+            {
+                unit.Health -= _damage;
+            }
+            else
+            {
+                Console.WriteLine("Unit is dead");
+            }
+
         }
 
     }
