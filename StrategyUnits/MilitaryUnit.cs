086a7ae65dde28c3ab5b820ae118c64b5ed6ef61
curiosity0902 <144440723+curiosity0902@users.noreply.github.com>
2024-01-17 22:39:06 +0300

    Update MilitaryUnit

--- StrategyUnits/MilitaryUnit.cs
+++ StrategyUnits/MilitaryUnit.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace StrategyUnits
 {
-    internal class MilitaryUnit
+    internal class MilitaryUnit: Unit
     {
+        private int _damage;
+
+        public int Damage
+        {
+            get { return _damage; }
+            set { _damage = value; }
+        }
+        public virtual void InflictDamage(Unit unit)
+        {
+            unit.Health -= _damage;
+            Console.WriteLine("You attaked.");
+        }
+
+        public MilitaryUnit(int health, string? name, int damage) : base(health, name)
+        {
+            _damage = damage;
+        }
     }
 }
