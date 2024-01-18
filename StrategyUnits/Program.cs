052d71706ebcdb9da3673b78ceeb9c1eae7a485f
202104 <202104@WINMAC-3>
2024-01-18 15:25:21 +0300

    Изменены делегаты и HealSomebody

--- StrategyUnits/Program.cs
+++ StrategyUnits/Program.cs
@@ -6,17 +6,26 @@ Healer healer = new Healer();
 Paladin paladin = new Paladin();
 Altar altar = new Altar();
 
+footman.HealthIncreasedEvent += HealthIncrease;
+footman.HealthDecreasedEvent += HealthDecrease;
+
 peasant.HealthIncreasedEvent += HealthIncrease;
 peasant.HealthDecreasedEvent += HealthDecrease;
 
-paladin.FireBall(peasant);
+healer.HealthIncreasedEvent += HealthIncrease;
+healer.HealthDecreasedEvent += HealthDecrease;
 
+paladin.HealthIncreasedEvent += HealthIncrease;
+paladin.HealthDecreasedEvent += HealthDecrease;
+
+paladin.FireBall(peasant);
+healer.HealSomebody(peasant);
 
 void HealthIncrease(string name, int health, int newHealth)
 {
-    Console.WriteLine($"Здоровье {name} равно {newHealth}");
+    Console.WriteLine($"Здоровье {name} увеличилось на {newHealth - health}. Текущее здоровье - {newHealth}");
 }
 void HealthDecrease(string name, int health, int newHealth)
 {
-    Console.WriteLine($"Здоровье {name} равно {newHealth}");
+    Console.WriteLine($"Здоровье {name} уменьшилось на {health - newHealth}. Текущее здоровье - {newHealth}");
 }
