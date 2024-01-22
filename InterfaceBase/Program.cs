cfa941ef12b67dc580335d2f7e04c3f9dee4cffe
202117 <202117@1220-011-22>
2024-01-22 11:09:14 +0300

    Создал решение Interface base

---
+++ InterfaceBase/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceBase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
