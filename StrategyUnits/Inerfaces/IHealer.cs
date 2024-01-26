0b9326ac6ff241a62f2225f1426ca52152a75f4a
Куликов Дмитрий Николаевич <202109@mck-ktits.ru>
2024-01-26 09:22:27 +0300

    added interfaces

---
+++ StrategyUnits/Inerfaces/IHealer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyUnits.Inerfaces
+{
+    internal interface IHealer
+    {
+        public void HealSomebody(IHealth currentHealth);
+    }
+}
---
+++ StrategyUnits/Inerfaces/IHealth.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyUnits.Inerfaces
+{
+    internal interface IHealth
+    {
+        public int Health { get; set; }
+    }
+}
