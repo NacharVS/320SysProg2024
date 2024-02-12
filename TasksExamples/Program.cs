7d827fc79d86abb64e1503147a8a932bc443e9d8
r3dinc <r3dinc@yandex.ru>
2024-02-12 10:18:54 +0300

    tasks

---
+++ TasksExamples/Program.cs
@@ -0,0 +1,26 @@
+﻿Task task1 = new Task(() =>
+{
+    for (int i = 0; i < 15; i++)
+    {
+        Console.WriteLine($"task {i}");
+        Thread.Sleep(500);
+    }
+});
+Task.WaitAll(
+    Task.Run(() =>
+    {
+        for (int i = 0; i < 15; i++)
+        {
+            Console.WriteLine($"task {i}");
+            Thread.Sleep(500);
+        }
+    })
+    );
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine($"main {i}");
+    Thread.Sleep(500);
+}
+
+
